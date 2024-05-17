bd19d6ace869af66a3eb70cb13655425ee093a9f
lknightnet <lknightnet@gmail.com>
2024-05-17 08:32:51 +0500

    2.0

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>KSP</title>
+    <script type="module" crossorigin src="{{ asset('assets/index-fa18e929.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/index-47f220bf.css') }}">
+</head>
+<body>
+<div id="root"></div>
+
+</body>
+</html>
