00f3737ec4f6cc8cf4c03f8871ca6bc35cf3cbfa
lknightnet <lknightnet@gmail.com>
2024-05-18 13:59:03 +0500

    4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>KSP</title>
-    <script type="module" crossorigin src="{{asset('/assets/index-eda34ea9.js')}}"></script>
-    <link rel="stylesheet" href="{{asset('/assets/index-343eaeed.css')}}">
+    <script type="module" crossorigin src="{{asset('/assets/index-f545efc7.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('/assets/index-41b95f3c.css')}}">
 </head>
 <body>
 <div id="root"></div>
